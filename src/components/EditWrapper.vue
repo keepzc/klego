a545c7bf881c0feeb24bcbe8f313d8d540c152be
mengbingzc <13699200852@163.com>
2023-01-08 00:45:52 +0800

    拖拽实现四个角拖拽大小

--- src/components/EditWrapper.vue
+++ src/components/EditWrapper.vue
@@ -3,10 +3,10 @@
     :class="{ active: active, hidden: hidden }">
     <slot></slot>
     <div class="resizers">
-      <div class='resizer top-left'></div>
-      <div class='resizer top-right'></div>
-      <div class='resizer bottom-left'></div>
-      <div class='resizer bottom-right' @mousedown.stop="startResize"></div>
+      <div class='resizer top-left' @mousedown.stop="startResize('top-left')"></div>
+      <div class='resizer top-right' @mousedown.stop="startResize('top-right')"></div>
+      <div class='resizer bottom-left' @mousedown.stop="startResize('bottom-left')"></div>
+      <div class='resizer bottom-right' @mousedown.stop="startResize('bottom-right')"></div>
     </div>
   </div>
 </template>
@@ -14,6 +14,14 @@
 <script lang="ts">
 import { defineComponent, computed, ref, nextTick } from 'vue'
 import { pick } from 'lodash-es'
+type ResizeDirection = 'top-left' | 'top-right' | 'bottom-left' | 'bottom-right'
+interface OriginalPositions {
+  left: number;
+  right: number;
+  top: number;
+  bottom: number;
+}
+
 export default defineComponent({
   props: {
     id: {
@@ -54,15 +62,63 @@ export default defineComponent({
         left
       }
     }
+    const caculateSize = (direction: ResizeDirection, e: MouseEvent, positions: OriginalPositions) => {
+      const { clientX, clientY } = e
+      const { left, right, top, bottom } = positions
+      const container = document.getElementById('canvas-area') as HTMLElement
+      const rightWidth = clientX - left
+      const leftWidth = right - clientX
+      const bottomHeight = clientY - top
+      const topHeight = bottom - clientY
+      const topOffset = clientY - container.offsetTop
+      const leftOffset = clientX - container.offsetLeft
+      switch (direction) {
+        case 'top-left':
+          return {
+            width: leftWidth,
+            height: topHeight,
+            left: leftOffset,
+            top: topOffset
+          }
+        case 'top-right':
+          return {
+            width: rightWidth,
+            height: topHeight,
+            top: topOffset
+          }
+        case 'bottom-left':
+          return {
+            width: leftWidth,
+            height: bottomHeight,
+            left: leftOffset
+          }
+        case 'bottom-right':
+          return {
+            width: rightWidth,
+            height: bottomHeight
+          }
+        default:
+          break;
+      }
+    }
     // 元素缩放
-    const startResize = (e: MouseEvent) => {
-      const currentElement = editWrapper.value
+    const startResize = (direction: ResizeDirection) => {
+      const currentElement = editWrapper.value as HTMLElement
+      const { left, right, top, bottom } = currentElement.getBoundingClientRect()
       const handleMove = (e: MouseEvent) => {
-        if (currentElement) {
-          const { left, top } = currentElement.getBoundingClientRect()
-          currentElement.style.width = e.clientX - left + 'px'
-          currentElement.style.height = e.clientY - top + 'px'
+        const size = caculateSize(direction, e, { left, right, top, bottom })
+        const { style } = currentElement
+        if (size) {
+          style.width = size.width + 'px'
+          style.height = size.height + 'px'
+          if (size.left) {
+            style.left = size.left + 'px'
+          }
+          if (size.top) {
+            style.top = size.top + 'px'
+          }
         }
+
       }
       const handleMouseUp = (e: MouseEvent) => {
         document.removeEventListener('mousemove', handleMove)
