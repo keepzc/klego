47814fa12121c9455d8db682d008852827943ee5
mengbingzc <13699200852@163.com>
2023-01-08 19:31:50 +0800

    新增editer.ts mutations 测试用例

--- tests/unit/store.spec.ts
+++ tests/unit/store.spec.ts
@@ -1,8 +1,10 @@
 import store from "@/store/index";
 import { testComponents, ComponentData } from "@/store/editor";
 import { testData } from "@/store/templates";
-import { clone, last } from "lodash-es";
+import { clone, last, cloneDeep } from "lodash-es";
+import { textDefaultProps } from 'kpzc-lego-components'
 const cloneComponents = clone(testComponents);
+jest.mock('ant-design-vue')
 describe("test vuex store", () => {
     it("should have three modules", () => {
         expect(store.state).toHaveProperty("editor");
@@ -43,14 +45,37 @@ describe("test vuex store", () => {
                 name: "l-text",
                 id: "123",
                 props: {
+                    ...textDefaultProps,
                     text: "text1"
                 }
             };
             store.commit("addComponent", payload);
             expect(store.state.editor.components).toHaveLength(cloneComponents.length + 1);
             const lastItem = last(store.state.editor.components);
-            expect(lastItem?.props.text).toBe("text1");
+            if(lastItem){
+                expect(lastItem.props.text).toBe("text1");
+                expect(lastItem.layerName).toBe('图层2')
+            }
+           
         });
+        it('copy & paste component should work fine', ()=>{
+            store.commit('copyComponent')
+            expect(store.state.editor.copiedComponent).toBeDefined()
+            store.commit('pastCopiedComponent')
+            expect(store.state.editor.components).toHaveLength(cloneComponents.length + 2);
+            const lastItem = last(store.state.editor.components);
+            if(lastItem){
+                expect(lastItem.props).toEqual(cloneComponents[0].props)
+            }
+        })
+        it('move component should works fine', () => {
+            const currentElement = store.getters.getCurrentElement as ComponentData
+            const oldValue = parseInt(currentElement.props.left || '0') 
+            store.commit('moveComponent', {direction: 'Left', amount: 5, id: currentElement.id})
+            expect(currentElement.props.left).toBe(oldValue - 5 + 'px')
+            store.commit('moveComponent', {direction: 'Right', amount: 3, id: currentElement.id})
+            expect(currentElement.props.left).toBe(oldValue - 5 + 3 + 'px')
+        })
         it("update component should work fine", () => {
             const newProps = {
                 key: "text",
@@ -59,6 +84,13 @@ describe("test vuex store", () => {
             store.commit("updateComponent", newProps);
             const currentElement: ComponentData = store.getters.getCurrentElement;
             expect(currentElement.props.text).toBe("update");
+            const newProps2 = {
+                key: 'layerName',
+                value: 'new layer',
+                isRoot: true
+            }
+            store.commit('updateComponent', newProps2)
+            expect(currentElement.layerName).toBe('new layer')
         });
     });
 });
