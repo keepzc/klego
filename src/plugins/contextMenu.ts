fd5f19dd62b0d3cfdb52205f380dccfb53cfa745
mengbingzc <13699200852@163.com>
2023-01-09 14:52:22 +0800

    丰富右键菜单功能

--- src/plugins/contextMenu.ts
+++ src/plugins/contextMenu.ts
@@ -5,7 +5,10 @@ import createContextMenu, { ActionItem} from '@/components/createContextMenu'
 const initContextMenu = () => {
     const store = useStore()
     const testActions: ActionItem[] = [
-        { shortcut: 'Backspace / Delete', text: '删除图层', action: (cid) => { store.commit('deleteComponent', cid) }}
+        { shortcut: '⌘C / Ctrl+C', text: '拷贝图层', action: (cid) => { store.commit('copyComponent', cid)} },
+        { shortcut: '⌘V / Ctrl+V',text: '粘贴图层', action: () => { store.commit('pastCopiedComponent')} },
+        { shortcut: 'Backspace / Delete', text: '删除图层', action: (cid) => { store.commit('deleteComponent', cid) } },
+        { shortcut: 'ESC',text: '取消选中', action: () => { store.commit('setActive', '')} },
     ]
     // 可以创建多个右键菜单 settings-panel
     // const testActions2: ActionItem[] = [
@@ -26,3 +29,38 @@ const initContextMenu = () => {
 }
 
 export default initContextMenu
+
+export const operationText: { [key: string]: {text: string; shortcut: string} } = {
+    copy: {
+      text: '拷贝图层',
+      shortcut: '⌘C / Ctrl+C'
+    },
+    paste: {
+      text: '粘贴图层',
+      shortcut: '⌘V / Ctrl+V'
+    },
+    delete: {
+      text: '删除图层',
+      shortcut: 'Backspace / Delete'
+    },
+    cancel: {
+      text: '取消选中',
+      shortcut: 'ESC'
+    },
+    undo: {
+      text: '撤销',
+      shortcut: '⌘Z / Ctrl+Z'
+    },
+    redo: {
+      text: '重做',
+      shortcut: '⌘⇧Z / Ctrl+Shift+Z'
+    },
+    move: {
+      text: '上下左右移动一像素',
+      shortcut: '↑ ↓ → ←'
+    },
+    moveTen: {
+      text: '上下左右移动十像素',
+      shortcut: 'Shift + ↑ ↓ → ←'
+    }
+}
