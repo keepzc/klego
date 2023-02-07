537bd711e0e174e0afc5c47440b2f7f8eeb2c2dc
mengbingzc <13699200852@163.com>
2023-02-07 14:31:51 +0800

    修改首页样式

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,11 @@
 <template>
-  <div class="content-container">
-    <a-row :gutter="16">
-      <template-list :list="testData"></template-list>
-    </a-row>
+  <div>
+    <template-list :list="testData"></template-list>
     <a-row type="flex" justify="center">
       <a-button type="primary" size="large" @click="loadMorePage" v-if="!isLastPage"
         :loading="isLoading">加载更多</a-button>
     </a-row>
-
   </div>
-
 </template>
 <script  lang="ts">
 import { defineComponent, computed, onMounted } from 'vue';
@@ -52,5 +48,12 @@ export default defineComponent({
 })
 </script>
 <style>
-
+.content-container {
+  background: #fff;
+  padding: 0 24px 24px 30px;
+  min-height: 85vh;
+  max-width: 1200px;
+  margin: 50px auto;
+  width: 100%;
+}
 </style>
